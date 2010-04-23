5e33f1dfead52fb838b952d2a41dcf5a23634485
Steve Losh <steve@stevelosh.com>
2010-04-23 11:20:24 -0400

    Add the cdp function.

--- lib/python.zsh
+++ lib/python.zsh
@@ -13,3 +13,9 @@ function cdp () {
 }
 
 alias cdv='cd $WORKON_HOME'
+
+function cdp () {
+  cd "$(python -c "import os.path as _, ${1}; \
+    print _.dirname(_.realpath(${1}.__file__[:-1]))"
+  )"
+}
